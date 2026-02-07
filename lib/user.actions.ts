7eed2ffdecbbfd65cd0c147e6594c7c13932d3d3
Prakhar0136 <prakharps010306@gmail.com>
2026-02-08 00:36:08 +0530

    implemented appwrite setup to create an user via signup and send emailOTP

---
+++ lib/user.actions.ts
@@ -0,0 +1,67 @@
+"use server";
+import { createAdminClient } from "./appwrite";
+import { appwriteConfig } from "./appwrite/config";
+import { Query, ID } from "node-appwrite";
+import { parseStringify } from "./utils";
+
+const getUserByEmail = async (email: string) => {
+  const { databases } = await createAdminClient();
+
+  const result = await databases.listRows({
+    databaseId: appwriteConfig.databaseId,
+    tableId: appwriteConfig.usersCollectionId,
+    queries: [Query.equal("email", email)],
+  });
+
+  return result.total > 0 ? result.rows[0] : null;
+};
+
+const handleError = (error: unknown, message: string) => {
+  console.log(error, message);
+  throw error;
+};
+
+const sendEmailOTP = async ({ email }: { email: string }) => {
+  const { account } = await createAdminClient();
+  try {
+    const session = await account.createEmailToken({
+      userId: ID.unique(),
+      email: email,
+    });
+
+    return session.userId;
+  } catch (error) {
+    handleError(error, "failed to send email OTP");
+  }
+};
+
+export const createAccount = async ({
+  fullName,
+  email,
+}: {
+  fullName: string;
+  email: string;
+}) => {
+  const existingUser = await getUserByEmail(email);
+
+  const accountId = await sendEmailOTP({ email });
+
+  if (!existingUser) {
+    const { databases } = await createAdminClient();
+
+    await databases.createRow({
+      databaseId: appwriteConfig.databaseId,
+      tableId: appwriteConfig.usersCollectionId,
+      rowId: ID.unique(),
+      data: {
+        fullName,
+        email,
+        avatar:
+          "https://imgs.search.brave.com/MMhX_fwxB_CXWHN83sjWFZ66JrnjfGoZ3WvI-5EAG6A/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvdGh1/bWJuYWlscy8wNDUv/OTQ0LzE5OS9zbWFs/bC9tYWxlLWRlZmF1/bHQtcGxhY2Vob2xk/ZXItYXZhdGFyLXBy/b2ZpbGUtZ3JheS1w/aWN0dXJlLWlzb2xh/dGVkLW9uLWJhY2tn/cm91bmQtbWFuLXNp/bGhvdWV0dGUtcGlj/dHVyZS1mb3ItdXNl/ci1wcm9maWxlLWlu/LXNvY2lhbC1tZWRp/YS1mb3J1bS1jaGF0/LWdyZXlzY2FsZS1p/bGx1c3RyYXRpb24t/dmVjdG9yLmpwZw",
+        accountId,
+      },
+    });
+  }
+
+  return parseStringify({ accountId });
+};
